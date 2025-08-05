430a418116b83fc8be5f0c2eddf2e4b5049fb617
Thalita Colofatti <62973671+thalitacolofatti@users.noreply.github.com>
2025-08-05 08:50:12 -0300

    Add adicionarAmigo function

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let nome = document.querySelector('input');
+    let amigo = nome.value;
+
+    if (amigo == '' || amigo == ' ') {
+        alert("Por favor, insira um nome.");
+    } else {
+        amigos.push(amigo);
+        nome.value = '';
+    }
+    // console.log(amigos);
+}
